b40c172ba918a7cd4125c139b068df31458e3ccb
JonathanADB <jhonnysoy1@gmail.com>
2024-05-14 14:25:26 +0200

    comenzamos

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('hola mundo lala ');
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath)
+
+//Server
+const app = express();
+//cargo la instanciua de express y donde quiero usar en que puerto que escuche 
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("servidor corriendo en puerto",app.get("port"));
+
+//Rutas
+app.get("/", (req, res)=> res.sendFile(__dirname + "/pages/login.html"))
