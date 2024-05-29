c586ba655f81343315311cbae52c2088ed5a5ca9
Jonathan Dominguez <150841815+JonathanADB@users.noreply.github.com>
2024-05-29 17:52:35 +0200

    Update admin.js

--- app/public/admin.js
+++ app/public/admin.js
@@ -1,4 +1,8 @@
-document.getElementsByTagName("button")[0].addEventListener("click",()=>{
-    document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.location.href = "/"
-  })
+// Selecciona el primer botón en el documento y añade un event listener para manejar el evento de clic
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  // Borra la cookie 'jwt' configurando su fecha de expiración en una fecha pasada
+  document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  
+  // Redirige el navegador a la página de inicio ("/")
+  document.location.href = "/";
+});
