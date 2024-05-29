9ba2c101d8b47ea57a7e6e44fa9613bab4a861bd
Jonathan Dominguez <150841815+JonathanADB@users.noreply.github.com>
2024-05-29 17:50:56 +0200

    Update login.js con comentarios para una mejor expocicion de la pildora

--- app/public/login.js
+++ app/public/login.js
@@ -1,21 +1,36 @@
-const mensajeError = document.getElementsByClassName("error")[0]
+// Selecciona el primer elemento en el documento con la clase "error" y lo asigna a la variable mensajeError
+const mensajeError = document.getElementsByClassName("error")[0];
 
-document.getElementById("login-form").addEventListener("submit",async (e)=>{
+// Añade un event listener al formulario con el ID "login-form" para manejar el evento de envío (submit)
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+  // Previene el comportamiento por defecto del formulario, que es recargar la página
   e.preventDefault();
+
+  // Obtiene los valores de los campos "user" y "password" del formulario
   const user = e.target.children.user.value;
   const password = e.target.children.password.value;
-  const res = await fetch("http://localhost:4000/api/login",{
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json"
+
+  // Realiza una solicitud HTTP POST asíncrona al servidor para iniciar sesión
+  const res = await fetch("http://localhost:4000/api/login", {
+    method: "POST", // Especifica el método HTTP como POST
+    headers: {
+      "Content-Type": "application/json" // Define el tipo de contenido de la solicitud como JSON
     },
     body: JSON.stringify({
-      user,password
+      // Convierte los datos del formulario en una cadena JSON
+      user,
+      password
     })
   });
-  if(!res.ok) return mensajeError.classList.toggle("escondido",false);
+
+  // Si la respuesta no es OK (status no es 200-299), muestra el mensaje de error
+  if (!res.ok) return mensajeError.classList.toggle("escondido", false);
+
+  // Convierte la respuesta del servidor a un objeto JSON
   const resJson = await res.json();
-  if(resJson.redirect){
+
+  // Si el JSON de la respuesta contiene una propiedad "redirect", redirige a la URL especificada
+  if (resJson.redirect) {
     window.location.href = resJson.redirect;
   }
-})
+});
