c8dc61e8fb59ddbeedb163c16f4abaa3c4ba472e
Jonathan Dominguez <150841815+JonathanADB@users.noreply.github.com>
2024-06-09 20:05:25 +0200

    Update authorization.js

--- app/middlewares/authorization.js
+++ app/middlewares/authorization.js
@@ -1,71 +1,51 @@
-// Importa los módulos necesarios: jsonwebtoken para manejar JWT y dotenv para manejar variables de entorno
+// Importa la librería jsonwebtoken para manejar JWTs
 import jsonwebtoken from "jsonwebtoken";
+// Importa la librería dotenv para manejar variables de entorno
 import dotenv from "dotenv";
-
-// Importa la lista de usuarios desde el controlador de autenticación
+// Importa el objeto 'usuarios' desde el controlador de autenticación
 import { usuarios } from "./../controllers/authentication.controller.js";
 
-// Configura dotenv para cargar variables de entorno desde un archivo .env
+// Carga las variables de entorno desde el archivo .env
 dotenv.config();
 
-/**
- * Middleware para permitir el acceso solo a administradores.
- * Revisa si el usuario está autenticado antes de permitir el acceso.
- * @param {Object} req - Objeto de solicitud HTTP.
- * @param {Object} res - Objeto de respuesta HTTP.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
+// Middleware para permitir el acceso solo a administradores
 function soloAdmin(req, res, next) {
-  const logueado = revisarCookie(req); // Verifica si el usuario está autenticado
-  if (logueado) return next(); // Si está autenticado, continúa con la solicitud
-  return res.redirect("/"); // Si no está autenticado, redirige a la página de inicio
+  // Revisa si el usuario está logueado revisando la cookie
+  const logueado = revisarCookie(req);
+  if (logueado) return next(); // Si está logueado, permite continuar con la siguiente función de middleware
+  return res.redirect("/"); // Si no está logueado, redirige a la página principal
 }
 
-/**
- * Middleware para permitir el acceso solo a usuarios no autenticados.
- * Revisa si el usuario no está autenticado antes de permitir el acceso.
- * @param {Object} req - Objeto de solicitud HTTP.
- * @param {Object} res - Objeto de respuesta HTTP.
- * @param {Function} next - Función para pasar al siguiente middleware.
- */
+// Middleware para permitir el acceso solo a usuarios no autenticados (público)
 function soloPublico(req, res, next) {
-  const logueado = revisarCookie(req); // Verifica si el usuario está autenticado
-  if (!logueado) return next(); // Si no está autenticado, continúa con la solicitud
-  return res.redirect("/admin"); // Si está autenticado, redirige a la página de administración
+  // Revisa si el usuario está logueado revisando la cookie
+  const logueado = revisarCookie(req);
+  if (!logueado) return next(); // Si no está logueado, permite continuar con la siguiente función de middleware
+  return res.redirect("/admin"); // Si está logueado, redirige a la página de administrador
 }
 
-/**
- * Función para revisar la cookie JWT y verificar la autenticidad del usuario.
- * @param {Object} req - Objeto de solicitud HTTP.
- * @returns {boolean} - Devuelve true si el usuario está autenticado, false en caso contrario.
- */
+// Función para revisar la cookie y verificar el JWT
 function revisarCookie(req) {
   try {
-    // Extrae la cookie JWT de las cookies de la solicitud
+    // Obtiene el valor de la cookie 'jwt' desde el encabezado de la solicitud
     const cookieJWT = req.headers.cookie.split("; ").find(cookie => cookie.startsWith("jwt=")).slice(4);
-    
-    // Verifica y decodifica el JWT utilizando la clave secreta almacenada en las variables de entorno
+    // Verifica y decodifica el token JWT usando la clave secreta
     const decodificada = jsonwebtoken.verify(cookieJWT, process.env.JWT_SECRET);
-    console.log(decodificada);
-    
-    // Busca al usuario decodificado en la lista de usuarios
+    console.log(decodificada); // Muestra la información decodificada en la consola
+    // Busca el usuario correspondiente en la lista de usuarios
     const usuarioAResvisar = usuarios.find(usuario => usuario.user === decodificada.user);
-    console.log(usuarioAResvisar);
-    
-    // Si el usuario no se encuentra en la lista, retorna false
+    console.log(usuarioAResvisar); // Muestra la información del usuario en la consola
     if (!usuarioAResvisar) {
-      return false;
+      return false; // Si el usuario no existe, retorna false
     }
-    // Si el usuario se encuentra en la lista, retorna true
-    return true;
+    return true; // Si el usuario existe, retorna true
   } catch {
-    // Si ocurre un error (p.ej., cookie no válida), retorna false
-    return false;
+    return false; // Si hay un error (ej. cookie no válida o no existe), retorna false
   }
 }
 
-// Exporta los métodos soloAdmin y soloPublico para su uso en otras partes de la aplicación
+// Exporta los métodos para ser utilizados en otras partes de la aplicación
 export const methods = {
   soloAdmin,
   soloPublico,
-};
+}
